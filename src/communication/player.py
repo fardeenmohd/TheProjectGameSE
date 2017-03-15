e748faa51a526ba13166c270a6d7e6c0be368f25
mohammed fardin <fardeenmohdpl@gmail.com>
2017-03-15 12:32:17 +0100

    First attempt on player

--- src/communication/player.py
+++ src/communication/player.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python
 from socket import *
+import datetime
 
-s = socket()
-host = "P21703"
-port = 1922
+DEFAULT_HOSTNAME = "localhost"
+DEFAULT_PORT = 420
 
-s.connect((host,port))
-received = s.recv(1024)
-print(received.decode())
-message = input()
-s.send(message.encode())
-received = s.recv(1024)
-print(received.decode())
 
-s.close
+class player:
+    """Represents a player"""
+    socket
+    id
+
+    def __init_(self, hostname=DEFAULT_HOSTNAME, port=DEFAULT_PORT):
+        self.hostname = DEFAULT_HOSTNAME
+        self.port = DEFAULT_PORT
+
+    def connect(self):
+        print("Creating a socket:" + datetime.datetime.now + "\n")
+        player.socket = socket()
+
+        while not socket.connect(self.hostname, self.port):
+            print("Trying to connect to the humble server:" + datetime.datetime.now + "\n")
+            if socket.connect(self.hostname, self.port):
+                print("Connected to the humble server:" + datetime.datetime.now + "\n")
+                received = socket.recv(1024)
+                player.id = received.decode()
+
+    def validate_type(given_type):
+        return True
+
+    def play(self):
+        while True:
+            try:
+                print("Please enter message type:")
+                message = input()
+                if self.validate_type(message):
+                    socket.send(message)
+                else:
+                    print("Please write a valid message type:")
+                    message.input()
+            except socket.timeout:
+                print("Disconnected from the humble server \n")
+                self.socket.close
