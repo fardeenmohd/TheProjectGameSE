e4236c2b0a94d99e0d76d2e57c1c4caaeb88a44d
Filip <filip.matracki@gmail.com>
2017-03-15 20:56:09 +0100

    added my own branch

--- src/communication/server.py
+++ src/communication/server.py
@@ -2,34 +2,44 @@
 from socket import *  # Import socket module
 from threading import *
 
+DEFAULT_PORT = 420
+DEFAULT_TIMEOUT = 10
+DEFAULT_CLIENT_LIMIT = 10
+
 
 class CommunicationServer:
-    def __init__(self, host=gethostname(), port=1922):
+
+    def __init__(self, host=gethostname(), port=DEFAULT_PORT):
         self.host = host
         self.port = port
         self.socket = socket()
         self.socket.bind((self.host, self.port))
-        print("Starting server.")
+        self.playerDict = {}
+        print("Starting server with hostname: ", self.host, " and on port ", self.port)
 
     def listen(self):
         self.socket.listen(1)  # Now wait for client connection.
+        player_id = 0
         while True:
             client, address = self.socket.accept()
             print("New client with address " + str(address) + " connected.")
-            client.settimeout(10)
-            Thread(target=self.listen_to_client, args=(client, address)).start()
+            self.playerDict[player_id] = client
+            player_id += 1
+            client.settimeout(DEFAULT_TIMEOUT)
+            Thread(target=self.handle_player, args=(client, address)).start()
 
-    def listen_to_client(self, client, address):
+    def handle_player(self, client, address):
         buffer_size = 1024
-        message = "Welcome to my server :)"
+
+        '''#I am just testing python black magic to find a given key given a value in a dictionary'''
+        message = "Hello player with id " + list(self.playerDict.keys())[list(self.playerDict.values()).index(client)]
         client.send(message.encode())
 
         while True:
             try:
                 received_data = client.recv(buffer_size)
-                print("Received: "+received_data.decode())
+                print("Server Received: "+received_data.decode())
                 if received_data:
-                    print("helo")
                     response = ("Your message was: "+received_data).encode()
                     client.send(response)
                 else:
@@ -39,6 +49,7 @@ class CommunicationServer:
                 return False
 
         client.close()
+
 
 def run():
     server = CommunicationServer()
