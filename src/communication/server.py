829fd4377476ae431245f9a9e970427556a1cfc7
matrackif <filip.matracki@gmail.com>
2017-03-16 11:46:29 +0100

    server checks for max client limit

--- src/communication/server.py
+++ src/communication/server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 from socket import *  # Import socket module
 from threading import *
+import time
 
 DEFAULT_PORT = 420
-DEFAULT_TIMEOUT = 10
+DEFAULT_TIMEOUT = 100
 DEFAULT_CLIENT_LIMIT = 10
 
 
@@ -15,18 +16,24 @@ class CommunicationServer:
         self.socket = socket()
         self.socket.bind((self.host, self.port))
         self.playerDict = {}
+        self.clientCount = 0
         print("Starting server with hostname: ", self.host, " and on port ", self.port)
 
     def listen(self):
         self.socket.listen(1)  # Now wait for client connection.
         player_id = 0
         while True:
-            client, address = self.socket.accept()
-            print("New client with address " + str(address) + " connected.")
-            self.playerDict[player_id] = client
-            player_id += 1
-            client.settimeout(DEFAULT_TIMEOUT)
-            Thread(target=self.handle_player, args=(client, address, player_id)).start()
+            if self.clientCount < DEFAULT_CLIENT_LIMIT:
+                client, address = self.socket.accept()
+                print("New client with address " + str(address) + " connected.")
+                self.playerDict[player_id] = client
+                self.clientCount += 1
+                player_id += 1
+                client.settimeout(DEFAULT_TIMEOUT)
+                Thread(target=self.handle_player, args=(client, address, player_id)).start()
+            else:
+                time.sleep(1)
+
 
     def handle_player(self, client, address, player_id):
         buffer_size = 1024
