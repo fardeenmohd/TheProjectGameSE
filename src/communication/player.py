57be7b447fe4f76a237d9f5e0b803cb5761562a2
xj <xjay12@gmail.com>
2017-03-17 16:16:46 +0100

    cleanup, more players runnable from one instance of player

--- src/communication/player.py
+++ src/communication/player.py
@@ -1,71 +1,92 @@
 #!/usr/bin/env python
-from socket import *
-import datetime
+import socket
 import time
+import threading
+from datetime import datetime
 
 
-DEFAULT_HOSTNAME = gethostname()
-DEFAULT_PORT = 420
+class Player:
+    """Represents a player (a client)"""
+    TIME_INTER_CONNECTION = 10
+    DEFAULT_HOSTNAME = socket.gethostname() #localhost
+    DEFAULT_PORT = 4000
+    DEFAULT_MESSAGE_BUFFER_SIZE = 1024
 
-
-class player:
-    """Represents a player"""
-
-    def __init__(self):
-        self.hostname = DEFAULT_HOSTNAME
-        self.port = DEFAULT_PORT
-        self.socket = socket()
+    def __init__(self, index, verbose):
+        # initialize client's fields to default values
+        self.hostname = Player.DEFAULT_HOSTNAME
+        self.port = Player.DEFAULT_PORT
+        self.socket = socket.socket()
+        self.index = index  # local index used to differentiate between players running in threads
+        self.id = None  # id used to communicate with server
+        self.verbose = verbose  # if yes, there will be a lot of output printed out
+        if self.verbose:
+            self.print_debug("Player successfully created.")
 
     def connect(self):
-        print(datetime.datetime.now().time())
-        print(":Creating a socket\n")
+        if self.verbose:
+            self.print_debug("Creating a socket")
 
         while True:
             try:
-                print(datetime.datetime.now().time())
-                print(":Trying to connect to the humble server")
+                if self.verbose:
+                    self.print_debug("Trying to connect to server...")
                 self.socket.connect((self.hostname, self.port))
-                print(datetime.datetime.now().time())
-                print(":Connected to the humble server")
-                received = socket.recv(self.socket, 1024)
+                if self.verbose:
+                    self.print_debug("Connected to server.")
+                received = self.socket.recv(Player.DEFAULT_MESSAGE_BUFFER_SIZE)
                 self.id = received.decode()
                 print(self.id)
                 return
 
-            except error:
-                print(datetime.datetime.now().time())
-                print("FAILED. Sleep briefly & try again")
-                time.sleep(10)
+            except socket.error:
+                if self.verbose:
+                    self.print_debug("Connecting to server failed. Trying again in "+Player.TIME_INTER_CONNECTION)
+                time.sleep(Player.TIME_INTER_CONNECTION)
                 continue
 
-    # def validate_type(self ,given_type):
-        # return True
+                # def validate_type(self ,given_type):
+                # return True
 
     def play(self):
         while True:
             try:
-                print("Please enter message type:")
-                message = input()
+                time.sleep(5)
+                message = "Hello server."
                 # if self.validate_type(self, message):
-                socket.send(self.socket, message.encode())
+                socket.socket.send(self.socket, message.encode())
                 received_data = self.socket.recv(1024)
-                print(str(datetime.datetime.now().time())+" - received from server: " + received_data.decode())
+                # print(str(datetime.now().time()) + " - received from server: " + received_data.decode())
+                if self.verbose:
+                    self.print_debug("Received from server:"+ str(received_data).decode())
                 if message == "close":
                     self.socket.close()
-                    print(str(datetime.datetime.now().time()) + " - Disconnected from the humble server ")
+                    if self.verbose:
+                        self.print_debug("Disconnected. ")
 
-                # else:
+                        # else:
                     # print("Please write a valid message type:")
 
             except ConnectionAbortedError:
                 self.socket.close()
-                print(str(datetime.datetime.now().time())+" - Disconnected from the humble server ")
+                if self.verbose:
+                    self.print_debug("Disconnected from server.")
                 return
 
+    def print_debug(self, message):
+        header = "Player " + str(self.index) + " at " + str(datetime.now().time()) + " - "
+        print(header, message)
 
 
+def run(number_of_players, verbose):
+    for i in range(number_of_players):
+        threading.Thread(target=create_player, args=(i + 1, verbose)).start()
 
-p = player()
-p.connect()
-p.play()
 
+def create_player(index, verbose=True):
+    p = Player(index, verbose)
+    p.connect()
+    p.play()
+
+
+run(3, True)
--- src/communication/server.py
+++ src/communication/server.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python
-from socket import *  # Import socket module
-from threading import *
+import socket
+import threading
 import time
 from datetime import datetime
 
-DEFAULT_PORT = 420
-DEFAULT_TIMEOUT = 10
-DEFAULT_CLIENT_LIMIT = 10
-
 
 class CommunicationServer:
-    def __init__(self, host=gethostname(), port=DEFAULT_PORT):
-        self.host = host
-        self.port = port
-        self.socket = socket()
+    DEFAULT_BUFFER_SIZE = 1024
+    DEFAULT_PORT = 4000
+    DEFAULT_TIMEOUT = 10
+    DEFAULT_CLIENT_LIMIT = 10
+
+    def __init__(self, verbose):
+        self.host = socket.gethostname() # LOCALHOST
+        self.port = CommunicationServer.DEFAULT_PORT
+        self.socket = socket.socket()
         self.socket.bind((self.host, self.port))
         self.playerDict = {}
         self.clientCount = 0
+        self.verbose = verbose
         print("Starting server with hostname: ", self.host, " and on port ", self.port)
 
     def listen(self):
         self.socket.listen()
-        Thread(target=self.print_state).start()
+        threading.Thread(target=self.print_state).start()
         print("Started listening")
         player_id = 0
         while True:
-            if self.clientCount < DEFAULT_CLIENT_LIMIT:
+            if self.clientCount < CommunicationServer.DEFAULT_CLIENT_LIMIT:
                 client, address = self.socket.accept()
                 print("New client with address " + str(address) + " connected.")
                 self.playerDict[player_id] = client
                 self.clientCount += 1
                 player_id += 1
-                client.settimeout(DEFAULT_TIMEOUT)
-                Thread(target=self.handle_player, args=(client, address, player_id)).start()
+                client.settimeout(CommunicationServer.DEFAULT_TIMEOUT)
+                threading.Thread(target=self.handle_player, args=(client, address, player_id)).start()
             else:
                 time.sleep(1)
 
@@ -42,7 +44,7 @@ class CommunicationServer:
             time.sleep(5)
 
     def handle_player(self, client, address, player_id):
-        buffer_size = 1024
+        buffer_size = CommunicationServer.DEFAULT_BUFFER_SIZE
         message = player_id
         client.send(str(message).encode())
 
@@ -53,15 +55,15 @@ class CommunicationServer:
                 response = ("Your message was: " + str(received_data))
                 client.send(response.encode())
 
-            except error:
+            except socket.error:
                 client.close()
                 self.clientCount -= 1
                 return False
 
 
-def run():
-    server = CommunicationServer()
+def run(verbose):
+    server = CommunicationServer(verbose)
     server.listen()
 
 
-run()
+run(True)
