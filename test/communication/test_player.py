eb5df403aabd052352ee43326237e72298bb3d84
fardeenmohd <fardeenmohdpl@gmail.com>
2017-03-22 19:59:15 +0100

    testing the filip xml error and some basic not working so far test

--- test/communication/test_player.py
+++ test/communication/test_player.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
-
 import pytest
-from src.communication import player, server
+import socket
+from threading import Thread
+
+from src.communication import player
 
 
-class PlayerTest:
-    def __init__(self):
-        self.guinea_player = player
-        self.guinea_server = server
+class TestPlayer:
+    DEFAULT_HOSTNAME = socket.gethostname()
+    DEFAULT_PORT = 420
+    DEFAULT_BUFFER_SIZE = 1024
 
-    def test_unit(self):
-        assert self.guinea_player.run() == 1
+    def run_fake_server(self):
+        server_sock = socket.socket()
+        server_sock.bind((self.DEFAULT_HOSTNAME, self.DEFAULT_PORT))
+        server_sock.listen(0)
+        server_sock.accept()
+        server_sock.close()
 
-    def test_integration(self):
-        assert self.guinea_server.run() == 1
+    def test_connect_and_disconnect(self):
+        server_thread = Thread(target=self.run_fake_server())
+        server_thread.start()
 
+        guinea_player = player
 
+        assert guinea_player.run(1, False, 1)
