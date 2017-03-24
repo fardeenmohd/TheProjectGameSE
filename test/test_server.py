e322615b89ab56819987e694e44dc6d14a954a02
matrackif <filip.matracki@gmail.com>
2017-03-24 14:13:11 +0100

    added ability to connect more than 1 mock_client to test server

--- test/test_server.py
+++ test/test_server.py
@@ -11,38 +11,55 @@ class TestServer:
         test.listen()
 
     def test_client_count(self):
-        mock_client, test_server = self.initialize_testing_environment()
+        mock_clients, test_server = self.initialize_testing_environment()
         assert test_server.clientCount == 1
-        self.close_testing_environment(mock_client, test_server)
+        self.close_testing_environment(mock_clients, test_server)
 
     def test_player_id(self):
-        mock_client, test_server = self.initialize_testing_environment()
-        received = mock_client.recv(test_server.DEFAULT_BUFFER_SIZE)
+        mock_clients, test_server = self.initialize_testing_environment()
+        received = mock_clients[0].recv(test_server.DEFAULT_BUFFER_SIZE)
         assert received.decode() == '0'
-        self.close_testing_environment(mock_client, test_server)
+        self.close_testing_environment(mock_clients, test_server)
 
-    def initialize_testing_environment(self):
+    def test_client_limit(self):
+        mock_clients, test_server = self.initialize_testing_environment(server.CommunicationServer.DEFAULT_CLIENT_LIMIT + 1)
+        print("Num of mock_clients: " + str(len(mock_clients)))
+        assert test_server.clientCount == test_server.DEFAULT_CLIENT_LIMIT
+        self.close_testing_environment(mock_clients, test_server)
+
+    def initialize_testing_environment(self, num_of_clients=1):
         """
         Creates a server that will be tested, as well as a single mock client that will connect to it. Returns both
         :return:
         """
+        mock_clients = []
         test_server = server.CommunicationServer(verbose=True, host=server.CommunicationServer.DEFAULT_HOSTNAME,
                                                  port=server.CommunicationServer.DEFAULT_PORT)
         server_thread = threading.Thread(target=self.run_server_to_be_tested, daemon=True, args=(test_server,))
         server_thread.start()
-        mock_client = socket.socket()
-        mock_client.connect((server.CommunicationServer.DEFAULT_HOSTNAME, server.CommunicationServer.DEFAULT_PORT))
-        time.sleep(0.1)
-        return mock_client, test_server
+        time.sleep(0.01)
+        for i in range(num_of_clients):
+            connected = False
+            mock_client = socket.socket()
+            mock_client.connect((server.CommunicationServer.DEFAULT_HOSTNAME, server.CommunicationServer.DEFAULT_PORT))
+            while not connected:
+                try:
+                    mock_client.connect((server.CommunicationServer.DEFAULT_HOSTNAME, server.CommunicationServer.DEFAULT_PORT))
+                    connected = True
+                except Exception as e:
+                    pass  # Do nothing, just try again
 
-    def close_testing_environment(self, mock_client, test_server):
+            mock_clients.append(mock_client)
+            # time.sleep(0.001) # sleep in order to allow client to properly connect
+
+
+        return mock_clients, test_server
+
+    def close_testing_environment(self, mock_clients, test_server):
         """
-         Shuts the mock client and server down
+         Shuts the mock clients and server down
         """
-        mock_client.close()
+        for mock_client in mock_clients:
+            mock_client.close()
         test_server.shutdown()
 
-
-tester = TestServer()
-tester.test_client_count()
-tester.test_player_id()
