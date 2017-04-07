c181b938e0a1842ae28e007b52239d6718610e63
xj <xjay12@gmail.com>
2017-04-07 15:00:26 +0200

    clean tests

--- src/communication/test_messages.py
+++ src/communication/test_messages.py
@@ -12,7 +12,7 @@ class TestClass(TestCase):
         player_guid = 12
         direction = 'up'
 
-        flag = False  # will be set to True if there is an error with the message.
+        flag = False  # will be set to True if the message doesn't fit with the schema.
         try:
             Move(game_id, player_guid, direction)
         except DocumentInvalid:
@@ -26,25 +26,9 @@ class TestClass(TestCase):
         generated_xml = GetGames()
         sample_xml = open("../messages/GetGames.xml").read()
 
-        assert generated_xml == sample_xml
+        flag = generated_xml == sample_xml
+        if not flag:
+            print("Generated:\n" + generated_xml)
+            print("Sample:\n" + sample_xml)
 
-    def test_placepiece(self):
-        # check if generated PlacePlace xml is the same as the example
-        game_id = 1
-        player_guid = 'c094cab7-da7b-457f-89e5-a5c51756035f'
-
-        generated_xml = PlacePiece(game_id, player_guid)
-        sample_xml = open("../messages/PlacePiece.xml").read()  # Discover, TestPiece, PlacePiece, PickUpPiece
-
-        assert generated_xml == sample_xml
-
-    def test_authorizeknowledgeexchange(self):
-        # check if generated PlacePlace xml is the same as the example
-        game_id = 1
-        player_guid = 'c094cab7-da7b-457f-89e5-a5c51756035f'
-        with_player_id = 3
-
-        generated_xml = AuthorizeKnowledgeExchange(game_id, player_guid, with_player_id)
-        sample_xml = open("../messages/AuthorizeKnowledgeExchange.xml").read()  # Discover, TestPiece, PlacePiece, PickUpPiece
-
-        assert generated_xml == sample_xml
+        assert flag
