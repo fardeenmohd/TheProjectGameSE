af391174a40ba6a5acd466b8b72ea165936eedea
matrackif <filip.matracki@gmail.com>
2017-05-08 14:18:35 +0200

    added stuff to test server

--- test/test_server.py
+++ test/test_server.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
-import socket
+
+import os
 from threading import Thread
 from time import sleep
 from unittest import TestCase
-
+os.chdir("../src/communication")
+from src.communication.info import ClientInfo
+from src.communication.player import Player
+from src.communication.gamemaster import GameMaster
 from src.communication import server
-
-
+#os.chdir("../../test")
 # from unittest.self.mock import Magicself.mock,patch,self.mock TODO maybe use this library for testing
 
 
 class TestServer(TestCase):
     def setUp(self):
-        self.mock_server = server.CommunicationServer(True)
+        self.testing_server = server.CommunicationServer(True)
 
     def test_listening(self):
         """
@@ -20,7 +23,7 @@ class TestServer(TestCase):
         """
         print("Client count test.")
 
-        listening_thread = Thread(target=self.mock_server.listen, daemon=True)
+        listening_thread = Thread(target=self.testing_server.listen, daemon=True)
         listening_thread.start()
         num_of_clients = 5
 
@@ -30,26 +33,29 @@ class TestServer(TestCase):
             # give the clients some time to actually connect:
             sleep(0.01)
 
-        assert self.mock_server.client_indexer == num_of_clients
+        assert self.testing_server.client_indexer == num_of_clients
 
     def connect_to_server(self):
-        mock_client = socket.socket()
-        mock_client.connect((self.mock_server.host, self.mock_server.port))
-        while True:
-            sleep(1)
+        """
+        Mock player connects to the server
+        :return:
+        """
+        p = Player(verbose=True)
+        if p.connect():
+            pass
 
     def test_send(self):
-        print("Test the send function.")
+        print("Testing the send function.")
 
-        listening_thread = Thread(target=self.mock_server.listen, daemon=True)
+        listening_thread = Thread(target=self.testing_server.listen, daemon=True)
         listening_thread.start()
-
-        mock_client = socket.socket()
-        mock_client.connect((self.mock_server.host, self.mock_server.port))
-
-        self.mock_server.send()
-
-        received = mock_client.recv(1024).decode()
+        mock_gm = GameMaster(verbose=True)
+        mock_gm.connect()
+        while len(self.testing_server.clients) == 0:
+            sleep(0.001)
+        print(str(self.testing_server.clients))
+        self.testing_server.send(self.testing_server.clients['0'], "hello")  # he should be the first client
+        received = mock_gm.receive()
         assert received == "hello"
 
     # def test_receive(self):
@@ -75,10 +81,10 @@ class TestServer(TestCase):
         DEFAULT_CLIENT_LIMIT amount of clients on the server
         :return:
         """
-        listening_thread = Thread(target=self.mock_server.listen, daemon=True)
+        listening_thread = Thread(target=self.testing_server.listen, daemon=True)
         listening_thread.start()
 
-        self.mock_server.clientLimit = 5
+        self.testing_server.clientLimit = 5
         num_of_clients = 5
 
         for i in range(num_of_clients):
@@ -87,13 +93,13 @@ class TestServer(TestCase):
             # give the clients some time to actually connect:
             sleep(0.01)
 
-        mock_client = socket.socket()
-        mock_client.connect((self.mock_server.host, self.mock_server.port))
+        mock_player = Player()
+        mock_player.connect()
 
-        assert self.mock_server.client_indexer == self.mock_server.clientLimit
+        assert self.testing_server.client_indexer == self.testing_server.clientLimit
 
     def tearDown(self):
         """
          Shuts the self.mock clients and server down
         """
-        self.mock_server.shutdown()
+        self.testing_server.shutdown()
