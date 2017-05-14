30da0a67554153f3baa4122a1fd17b8b50aee69a
fardeenmohd <fardeenmohdpl@gmail.com>
2017-05-15 01:07:09 +0200

    +some magic!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/test/test_strategy.py
+++ src/test/test_strategy.py
@@ -76,15 +76,15 @@ class TestStrategy(TestCase):
     def test_try_collision(self):
 
         starting_location = (0, 2)
-        self.game_info.goal_fields[0, 3].player_id = "1"
-        self.game_info.task_fields[0, 1].player_id = "2"
+        self.red_strategy.game_info.goal_fields[0, 3].player_id = "1"
+        self.red_strategy.game_info.task_fields[0, 1].player_id = "2"
         self.red_strategy.current_location = starting_location
         self.red_strategy.last_move = Decision(Decision.DISCOVER)
-        red_decision = self.red_strategy.get_next_move(starting_location)
+        red_decision = self.red_strategy.try_go_down()
         print("Testing the red player to not move on same task field as any other player ")
         print("Got this decision: " + str(red_decision.choice) + ", additional info: " + str(
             red_decision.additional_info))
-        assert red_decision.additional_info == Direction.RIGHT.value
+        assert red_decision.additional_info != Direction.DOWN.value
 
     def test_valid_place(self):
         self.game_info.pieces["1"] = PieceInfo("1", PieceType.NORMAL)
