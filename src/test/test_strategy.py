b98dc5c67b1674d6cdd256e81290b2d5e52a1dc7
fardeenmohd <fardeenmohdpl@gmail.com>
2017-05-14 18:52:30 +0200

    + a lil addition to the strategy tests - placing has a glitch

--- src/test/test_strategy.py
+++ src/test/test_strategy.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from src.communication import strategy
-from src.communication.info import GameInfo, Allegiance, Direction
+from src.communication.info import GameInfo, Allegiance, Direction, GoalFieldType, PieceInfo, PieceType
 from src.communication.strategy import Decision
 
 print("Lookup: NULLDECISION = 0, MOVE = 1, DISCOVER = 5, KNOWLEDGE_EXCHANGE = 6, PICK_UP = 7, PLACE = 8")
@@ -14,7 +14,6 @@ class TestStrategy(TestCase):
 
         self.game_info = GameInfo(board_width=2, task_height=2, goals_height=1)
         self.game_info.initialize_fields()
-        self.game_info.add_piece("1", 1, 1)
 
         self.mock_strategy = strategy.StrategyFactory(Allegiance.RED.value, game_info=self.game_info)
         print()
@@ -37,9 +36,34 @@ class TestStrategy(TestCase):
 
     def test_move_toward_piece(self):
         # player is in a task field with current information, choice should be to move
+        self.game_info.add_piece("1", 1, 1)
         starting_location = (0, 2)  # this is a task field.
         self.mock_strategy.last_move = Decision(Decision.DISCOVER)
         print("Testing moving to piece. Expecting player to Move (to a piece).")
         decision = self.mock_strategy.get_next_move(starting_location)
         print("Got this decision: " + str(decision.choice) + ", additional info: " + str(decision.additional_info))
         assert decision.choice == Decision.MOVE
+
+    def test_valid_pickup(self):
+        self.game_info.add_piece("1", 0, 2)  # adding a piece
+        starting_location = (0, 2)  # this is a task field
+        self.mock_strategy.last_move = Decision(Decision.DISCOVER)
+        print("Testing picking a piece. Expecting player to Pick(a piece).")
+        decision = self.mock_strategy.get_next_move(starting_location)
+        print("Got this decision: " + str(decision.choice) + ", additional info: " + str(decision.additional_info))
+        assert decision.choice == Decision.PICK_UP
+
+    def test_valid_place(self):
+        self.game_info.pieces["1"] = PieceInfo("1", PieceType.NORMAL, "1")
+        starting_location = (0, 0)
+        self.mock_strategy.have_piece = "1"
+        self.game_info.goal_fields[0, 0].type = GoalFieldType.GOAL.value
+        self.mock_strategy.last_move = Decision(Decision.DISCOVER)
+        print("Testing placing a piece. Expecting player to Place(a piece).")
+        decision = self.mock_strategy.get_next_move(starting_location)
+        print("Got this decision: " + str(decision.choice) + ", additional info: " + str(decision.additional_info))
+        assert decision.choice == Decision.PLACE
+
+
+
+
