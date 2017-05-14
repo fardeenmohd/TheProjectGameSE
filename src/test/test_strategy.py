362fac45f7de39531c0bcce91108157da58e9c4a
fardeenmohd <fardeenmohdpl@gmail.com>
2017-05-15 01:16:47 +0200

    +(some magic!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)^2

--- src/test/test_strategy.py
+++ src/test/test_strategy.py
@@ -81,10 +81,15 @@ class TestStrategy(TestCase):
         self.red_strategy.current_location = starting_location
         self.red_strategy.last_move = Decision(Decision.DISCOVER)
         red_decision = self.red_strategy.try_go_down()
+
+        while red_decision.additional_info == Direction.DOWN.value:
+            red_decision =self.red_strategy.get_next_move(starting_location)
+
         print("Testing the red player to not move on same task field as any other player ")
         print("Got this decision: " + str(red_decision.choice) + ", additional info: " + str(
             red_decision.additional_info))
         assert red_decision.additional_info != Direction.DOWN.value
+
 
     def test_valid_place(self):
         self.game_info.pieces["1"] = PieceInfo("1", PieceType.NORMAL)
